d0a30af258584dac14e07c3434842cb2eeeb565e
kjolson12 <kobaseball02@gmail.com>
2021-03-24 11:05:58 -0600

    added edge cases to tribonacci

--- tribonacci.js
+++ tribonacci.js
@@ -2,43 +2,22 @@ function tribonacci(sig, n) {
     let reduceArr = [...sig];
     let count = n - 3;
 
+    if (n <= 3) {
+        return sig.slice(0, n);
+    }
+
     while (count > 0) {
         const sum = reduceArr.reduce((a, b) => a + b);
-        console.log(`sum: ${sum}`);
 
         sig.push(sum);
-        console.log(`sig: ${sig}`);
 
         reduceArr.push(sum);
         reduceArr.shift();
-        console.log(`reduceArr: ${reduceArr}`);
 
         count--;
     }
 
     return sig;
 }
-
-// function tribonacci(sig, n) {
-//     let returnArr = [1, 1, 1];
-//     let reduceArr = [1, 1, 1];
-//     let count = n - 3;
-
-//     while (count > 0) {
-//         const sum = reduceArr.reduce((a, b) => a + b);
-//         console.log(`sum: ${sum}`);
-
-//         returnArr.push(sum);
-//         console.log(`returnArr: ${returnArr}`);
-
-//         reduceArr.push(sum);
-//         reduceArr.shift();
-//         console.log(`reduceArr: ${reduceArr}`);
-
-//         count--;
-//     }
-
-//     return returnArr;
-// }
 
 tribonacci([1, 1, 1], 10);
